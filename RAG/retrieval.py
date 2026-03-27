7994d143439053eb1de63031d9d2237bd01d75bf
Vignesh D <vignesh.d9123@gmail.com>
2026-03-27 22:33:11 +0530

    Add RAG implementation without LCEL

--- RAG/retrieval.py
+++ RAG/retrieval.py
@@ -1,7 +1,9 @@
+from operator import itemgetter
 import os
 
 from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 
@@ -32,20 +34,42 @@ Question: {question}
 Provide a detailed answer:"""
 )
 
+def format_docs(relevant_docs):
+    print("Relevant docs", relevant_docs)
+    return "\n\n".join(doc.page_content for doc in relevant_docs)
+
+def rag_without_lcel(query: str):   
+    relevant_docs = retriever.invoke(query)
+    print("\n\nRelevant docs are", relevant_docs)
+    context = format_docs(relevant_docs)
+    messages = prompt_template.format_messages(context=context, question=query)
+    print("\n\nMessages sending to llm", messages)
+    rag_response = llm.invoke(messages)
+    print("\n\nRag implementation response", rag_response)
+
+def rag_with_lcel():
+    chain = (
+        RunnablePassthrough.assign(
+            context=itemgetter("question") | retriever | format_docs
+        )
+        | prompt_template
+        | llm
+    )
+    return chain
+
 query = "what is Openclaw, explain in detail? and how to implement safety in this"
 
 raw_response = llm.invoke(query)
 
 print("\n\nRaw response", raw_response)
 
-relevant_docs = retriever.invoke(query)
+rag_without_lcel(query)
 
-print("\n\nRelevant docs are", relevant_docs)
-
-context = "\n\n".join(doc.page_content for doc in relevant_docs)
-
-messages = prompt_template.format_messages(context=context, question=query)
-print("\n\nMessages sending to llm", messages)
-rag_response = llm.invoke(messages)
-
-print("\n\nRag implementation response", rag_response)
+lcel_chain = rag_with_lcel()
+print("\n\n")
+print("RAG WITHOUT LCEL")
+print("="*50)
+response = lcel_chain.invoke({
+    "question" : query
+})
+print(response.content)
